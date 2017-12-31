8538c0bc4aa9fba0393f23846f33749405d3eee8
A.Radmanesh <radmanesh@gmail.com>
2017-12-31 04:14:50 +0330

    Updated minure try to fix arabic numbers

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source "https://rubygems.org"
 gemspec
 
 group :jekyll_plugins do
-  gem "jekyll-hinduarabic"
   gem "jekyll-jalali"
   gem 'jekyll-default-layout'
   gem "jekyll-tagging", :require => 'jekyll/tagging'
--- _plugins/jekyll-hinduarabic.rb
+++ _plugins/jekyll-hinduarabic.rb
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
+
 module Jekyll
   module HinduArabicFilter
 
     def habify(input)
-      source = {"1" => "۱", "2" => "۲", "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
+      source = {"1" => "۱".force_encoding('utf-8'), "2" => "۲".force_encoding('utf-8'), "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
       for x in source.keys
         if input.include? x
           input = input.gsub(x, source[x])
         end
       end
-      return input
+      return input.force_encoding('utf-8')
     end
   end
 end
