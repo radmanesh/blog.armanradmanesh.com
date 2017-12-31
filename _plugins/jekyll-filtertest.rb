2b39becbcffcf570d48e486e330e2e391c6fe10c
A.Radmanesh <radmanesh@gmail.com>
2018-01-01 02:37:06 +0330

    try to fix arabic numbers

--- _plugins/jekyll-filtertest.rb
+++ _plugins/jekyll-filtertest.rb
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 module Jekyll
-  module TestFilter
+  module TestingFilter
 
-    def test(input)
-      if input==nil
-        return "nil"
+    def testing(input)
+      source = {"1" => "۱", "2" => "۲", "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
+      for x in source.keys
+        if input.include? x
+          input = input.gsub(x, source[x])
+        end
       end
       return input
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::TestFilter)
+Liquid::Template.register_filter(Jekyll::TestingFilter)
 
