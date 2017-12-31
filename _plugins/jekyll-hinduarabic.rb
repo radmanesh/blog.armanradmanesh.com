fcf9061c8644ce3cc5f0133b93ca8613eaade8b2
A.Radmanesh <radmanesh@gmail.com>
2017-12-31 07:59:14 +0330

    try to fix arabic numbers

--- _plugins/jekyll-hinduarabic.rb
+++ _plugins/jekyll-hinduarabic.rb
@@ -4,13 +4,13 @@ module Jekyll
   module HinduArabicFilter
 
     def habify(input)
-      source = {"1" => "۱".force_encoding('utf-8'), "2" => "۲".force_encoding('utf-8'), "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
+      source = {"1" => "۱", "2" => "d", "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
       for x in source.keys
         if input.include? x
           input = input.gsub(x, source[x])
         end
       end
-      return input.force_encoding('utf-8')
+      return input
     end
   end
 end
