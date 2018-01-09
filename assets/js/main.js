1d20c2cde5f2986480086711729a36d0f758be14
A.Radmanesh <radmanesh@gmail.com>
2018-01-10 02:21:35 +0330

    Added widgetpack commenting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-// This file contains JavaScript code required to display some bits on http://mehdix.ir
-
-// main.js
-
-// Central global variable
-var Mehdix = {};
-
-
-
-// Get code tags and change their dir attribute to ltr.
-// TODO: Find a solution to generate this during the build
-var codeTags = document.getElementsByTagName('code');
-for (var i = 0; i < codeTags.length; i++) {
-	codeTags[i].setAttribute('dir', 'ltr');
-}
+// $(document).on('DOMSubtreeModified','#wpac-comment',function(){
+//   $('#wpac-comment .wp-copyright').hide();
+// });
